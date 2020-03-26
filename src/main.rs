cb1c4e1bc8892b858161f13939ebebbb45d478a8
Danny Meyer <Danny.Meyer@gmail.com>
2020-03-26 12:45:06 -0400

    app server

---
+++ src/main.rs
@@ -0,0 +1,105 @@
+mod server;
+use crate::server::types::{Language, RenderError, RenderSuccess};
+use crate::server::{get_file, render, single_page_app};
+use actix_cors::Cors;
+use actix_files::{Files, NamedFile};
+use actix_rt;
+use actix_web::{body::Body, http::StatusCode, web, App, HttpRequest, HttpResponse, HttpServer};
+
+use mime_guess::from_path;
+use rust_embed::RustEmbed;
+use std::env;
+use std::path::PathBuf;
+use std::{borrow::Cow, sync::mpsc, thread};
+use web_view;
+use web_view::Content;
+use weresocool::generation::{RenderReturn, RenderType};
+use weresocool::interpretable::{InputType, Interpretable};
+use weresocool_error::ErrorInner;
+
+#[derive(RustEmbed)]
+#[folder = "src/server/build"]
+struct Asset;
+
+fn assets(req: HttpRequest) -> HttpResponse {
+    let path = if req.path() == "/" {
+        // if there is no path, return default file
+        "index.html"
+    } else {
+        // trim leading '/'
+        &req.path()[1..]
+    };
+    dbg!(&path);
+
+    // query the file from embedded asset with specified path
+    match Asset::get(path) {
+        Some(content) => {
+            let body: Body = match content {
+                Cow::Borrowed(bytes) => bytes.into(),
+                Cow::Owned(bytes) => bytes.into(),
+            };
+            HttpResponse::Ok()
+                .content_type(from_path(path).first_or_octet_stream().as_ref())
+                .body(body)
+        }
+        None => HttpResponse::NotFound().body("404 Not Found"),
+    }
+}
+
+#[actix_rt::main]
+pub async fn main() -> Result<(), actix_web::Error> {
+    std::env::set_var("RUST_LOG", "socool_server=info, actix_web=info");
+    env_logger::init();
+
+    let port = env::var("PORT")
+        .unwrap_or_else(|_| "4599".to_string())
+        .parse()
+        .expect("PORT must be a number");
+    println!("Listening on {}", &port);
+
+    let (server_tx, server_rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let sys = actix_rt::System::new("WereSoCool Server");
+        let server = HttpServer::new(|| {
+            App::new()
+                .wrap(Cors::new().finish())
+                .service(
+                    web::scope("/api")
+                        .route("/render", web::post().to(render))
+                        .route("/songs/{filename:.*}", web::get().to(get_file)),
+                )
+                .route("/compose", web::get().to(single_page_app))
+                .route("/play/{filename:.*}", web::get().to(single_page_app))
+                .default_service(web::get().to(assets))
+        })
+        .bind(("0.0.0.0", port))
+        .unwrap();
+
+        //let port = server.addrs().first().unwrap().port();
+        let server = server.run();
+
+        let _ = server_tx.send(server);
+
+        let _ = sys.run();
+    });
+
+    let server = server_rx.recv().unwrap();
+    //let port = port_rx.recv().unwrap();
+
+    web_view::builder()
+        .title("WereSoCool")
+        .content(Content::Url(format!("http://localhost:{}", port)))
+        .size(600, 800)
+        .resizable(true)
+        .debug(true)
+        .user_data(())
+        .invoke_handler(|_webview, _arg| Ok(()))
+        .run()
+        .unwrap();
+
+    // gracefully shutdown actix web server
+    let _ = server.stop(true).await;
+    println!("Shutdown");
+    Ok(())
+}
---
+++ src/server/mod.rs
@@ -0,0 +1,71 @@
+pub mod types;
+use crate::server::types::{Language, RenderError, RenderSuccess};
+use actix_cors::Cors;
+use actix_files::{Files, NamedFile};
+use actix_web::{http::StatusCode, web, App, HttpRequest, HttpResponse, HttpServer};
+use std::env;
+use std::path::PathBuf;
+use weresocool::generation::{RenderReturn, RenderType};
+use weresocool::interpretable::{InputType, Interpretable};
+use weresocool_error::ErrorInner;
+
+pub async fn get_file(req: HttpRequest) -> actix_web::Result<NamedFile> {
+    let mut path = PathBuf::from("../sound/renders");
+    let file: PathBuf = req.match_info().query("filename").parse().unwrap();
+    path.push(file);
+    Ok(NamedFile::open(path)?)
+}
+
+pub async fn single_page_app(_req: HttpRequest) -> actix_web::Result<NamedFile> {
+    let path = PathBuf::from("./src/server/build/index.html");
+    Ok(NamedFile::open(path)?)
+}
+
+pub async fn render(req: web::Json<Language>) -> HttpResponse {
+    let result = InputType::Language(&req.language).make(RenderType::StereoWaveform);
+    match result {
+        Ok(render_return) => match render_return {
+            RenderReturn::StereoWaveform(wav) => HttpResponse::Ok()
+                .content_type("application/json")
+                .status(StatusCode::OK)
+                .json(RenderSuccess::new(wav)),
+            _ => panic!(),
+        },
+        Err(parse_error) => {
+            let inner = *parse_error.inner;
+            match inner {
+                ErrorInner::ParseError(error) => HttpResponse::Ok()
+                    .content_type("application/json")
+                    .status(StatusCode::OK)
+                    .json(RenderError::new(error)),
+                _ => panic!(),
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::{http::header, test, web, App};
+
+    #[actix_rt::test]
+    async fn test_index() {
+        let language = Language {
+            language: "{f: 100, l: 1, g: 1, p: 0}\nmain={Tm 1}\n".to_string(),
+        };
+        let req = test::TestRequest::post()
+            .uri("/api/render")
+            .header(header::CONTENT_TYPE, "application/json")
+            .set_json(&language)
+            .to_request();
+
+        let mut app = test::init_service(
+            App::new().service(web::resource("/api/render").route(web::post().to(render))),
+        )
+        .await;
+
+        let resp = test::call_service(&mut app, req).await;
+        assert!(resp.status().is_success());
+    }
+}
